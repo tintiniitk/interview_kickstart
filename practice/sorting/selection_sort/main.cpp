cc013749f07bb0afba865cfcd3f08f11864bd6da
Nitin Garg <tintin.iitk@gmail.com>
2026-04-08 20:40:36 +0530

    switch from recursive to iterative

--- practice/sorting/selection_sort/main.cpp
+++ practice/sorting/selection_sort/main.cpp
@@ -14,31 +14,27 @@ ostream& operator<<(ostream& o, const v& input) {
 	return o;
 }
 
-bool selection_sort_intermediate(v &input, int start, int end) {
-	if (end <= (start + 1)) {
-		return true;
-	}
-	//cout << "At start = " << start << ", end = " << end << ", arr = \t\t\t" << input << endl;
-	auto min = input[start];
-	int min_index = start;
-	for (int i = start + 1; i < end; ++i) {
-		if (input[i] < min) {
-			min_index = i;
-			min = input[i];
-		}
-	}
-	if (min_index != start) {
-		input[min_index] = input[start];
-		input[start] = min;
-	}
-	return selection_sort_intermediate(input, start + 1, end);
-}
-
 bool selection_sort(v &input) {
 	if (input.size() < 2) {
 		return true;
 	}
-	return selection_sort_intermediate(input, 0, input.size());
+	int end = input.size();
+	for (int start = 0 ; start < end - 1; ++start) {
+		//cout << "At start = " << start << ", end = " << end << ", arr = \t\t\t" << input << endl;
+		auto min = input[start];
+		int min_index = start;
+		for (int i = start + 1; i < end; ++i) {
+			if (input[i] < min) {
+				min_index = i;
+				min = input[i];
+			}
+		}
+		if (min_index != start) {
+			input[min_index] = input[start];
+			input[start] = min;
+		}
+	}
+	return true;
 }
 
 int main(int argc, char *argv[]) {
