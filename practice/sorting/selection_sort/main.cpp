989ac96c9185d19481db9ecd5011a46f472b3959
Nitin Garg <tintin.iitk@gmail.com>
2026-04-10 23:00:23 +0530

    format code

--- practice/sorting/selection_sort/main.cpp
+++ practice/sorting/selection_sort/main.cpp
@@ -6,41 +6,42 @@ using namespace std;
 using v = vector<int>;
 
 ostream& operator<<(ostream& o, const v& input) {
-	o << "[ ";
-	for (auto elem: input) {
-		cout << elem << ", ";
-	}
-	o << " ]";
-	return o;
+    o << "[ ";
+    for (auto elem : input) {
+        cout << elem << ", ";
+    }
+    o << " ]";
+    return o;
 }
 
-bool selection_sort(v &input) {
-	if (input.size() < 2) {
-		return true;
-	}
-	int end = input.size();
-	for (int start = 0 ; start < end - 1; ++start) {
-		//cout << "At start = " << start << ", end = " << end << ", arr = \t\t\t" << input << endl;
-		auto min = input[start];
-		int min_index = start;
-		for (int i = start + 1; i < end; ++i) {
-			if (input[i] < min) {
-				min_index = i;
-				min = input[i];
-			}
-		}
-		if (min_index != start) {
-			input[min_index] = input[start];
-			input[start] = min;
-		}
-	}
-	return true;
+bool selection_sort(v& input) {
+    if (input.size() < 2) {
+        return true;
+    }
+    int end = input.size();
+    for (int start = 0; start < end - 1; ++start) {
+        // cout << "At start = " << start << ", end = " << end << ", arr =
+        // \t\t\t" << input << endl;
+        auto min = input[start];
+        int min_index = start;
+        for (int i = start + 1; i < end; ++i) {
+            if (input[i] < min) {
+                min_index = i;
+                min = input[i];
+            }
+        }
+        if (min_index != start) {
+            input[min_index] = input[start];
+            input[start] = min;
+        }
+    }
+    return true;
 }
 
-int main(int argc, char *argv[]) {
-	v input{5,3,2,4,9,6,7,1,0,8};
-	cout << "Before sorting: \t\t\t\t" << input << endl;
-	selection_sort(input);
-	cout << "After sorting: \t\t\t\t\t" << input << endl;
-	return 0;
+int main(int argc, char* argv[]) {
+    v input{5, 3, 2, 4, 9, 6, 7, 1, 0, 8};
+    cout << "Before sorting: \t\t\t\t" << input << endl;
+    selection_sort(input);
+    cout << "After sorting: \t\t\t\t\t" << input << endl;
+    return 0;
 }
--- practice/sorting/selection_sort/main.py
+++ practice/sorting/selection_sort/main.py
@@ -2,7 +2,7 @@ def selection_sort(v):
     if len(v) < 2:
         return True
     end = len(v)
-    for start in range(0, end-1):
+    for start in range(0, end - 1):
         # print(f"At start = {start}, end = {end} arr = \t\t\t{v}")
         min_value = v[start]
         min_index = start
@@ -14,11 +14,13 @@ def selection_sort(v):
             v[min_index] = v[start]
             v[start] = min_value
 
+
 def main():
-    v = [5,3,2,4,9,6,7,1,0,8]
+    v = [5, 3, 2, 4, 9, 6, 7, 1, 0, 8]
     print(f"Before sorting: \t\t\t\t{v}")
     selection_sort(v)
     print(f"After sorting: \t\t\t\t\t{v}")
+
 
 if __name__ == "__main__":
     main()
