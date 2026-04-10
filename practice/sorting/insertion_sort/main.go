7b9ca2bef9e2e1da0625fdac02ce3d85bb8881b2
Nitin Garg <tintin.iitk@gmail.com>
2026-04-11 04:30:13 +0530

    commit improvement in insertion-sort algo

--- practice/sorting/insertion_sort/main.go
+++ practice/sorting/insertion_sort/main.go
@@ -17,22 +17,17 @@ func insertion_sort(arr ints) (ints, int, int) {
 	num_swaps := 0
 	for i := 1; i < n; i++ {
 		val := arr[i]
-		index := i
-		for j := 0; j < i; j++ {
-			num_comps++
-			if new_arr[j] > val {
-				index = j
-				break
+		index := i - 1
+		new_arr = append(new_arr, val)
+		num_comps++
+		for index >= 0 && new_arr[index] > val {
+			new_arr[index+1] = new_arr[index]
+			index--
+			if index >= 0 {
+				num_comps++
 			}
 		}
-		new_arr = append(new_arr, val)
-		for k := i; k > index; k-- {
-			//num_swaps++
-			//temp := new_arr[k]
-			new_arr[k] = new_arr[k-1]
-			//new_arr[k-1] = temp
-		}
-		new_arr[index] = val
+		new_arr[index+1] = val
 	}
 	return new_arr, num_swaps, num_comps
 }
@@ -51,19 +46,19 @@ func main() {
 			arr:                      ints{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
 			expected_sorted_arr:      ints{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
 			expected_num_swaps:       0,
-			expected_num_comparisons: 45,
+			expected_num_comparisons: 9,
 		}, {
 			name:                     "tc2-reverse-sorted",
 			arr:                      ints{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
 			expected_sorted_arr:      ints{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
 			expected_num_swaps:       0,
-			expected_num_comparisons: 9,
+			expected_num_comparisons: 45,
 		}, {
 			name:                     "tc3-unsorted",
 			arr:                      ints{5, 3, 2, 4, 9, 6, 7, 1, 0, 8},
 			expected_sorted_arr:      ints{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
 			expected_num_swaps:       0,
-			expected_num_comparisons: 31,
+			expected_num_comparisons: 27,
 		},
 	}
 	for _, tc := range test_cases {
