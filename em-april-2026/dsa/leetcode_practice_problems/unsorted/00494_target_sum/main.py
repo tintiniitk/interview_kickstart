aa76f03fc408a74f2a007bae9e3748dcb758aec5
Nitin Garg <tintin.iitk@gmail.com>
2026-04-26 23:37:58 +0530

    add python code for leetcode #494 target_sum

---
+++ em-april-2026/dsa/leetcode_practice_problems/unsorted/00494_target_sum/main.py
@@ -0,0 +1,66 @@
+# https://leetcode.com/problems/target-sum/description/
+
+"""
+
+You are given an integer array nums and an integer target.
+
+You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+
+For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
+Return the number of different expressions that you can build, which evaluates to target.
+
+ Example 1:
+
+ Input: nums = [1,1,1,1,1], target = 3
+ Output: 5
+ Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+ -1 + 1 + 1 + 1 + 1 = 3
+ +1 - 1 + 1 + 1 + 1 = 3
+ +1 + 1 - 1 + 1 + 1 = 3
+ +1 + 1 + 1 - 1 + 1 = 3
+ +1 + 1 + 1 + 1 - 1 = 3
+ Example 2:
+
+ Input: nums = [1], target = 1
+ Output: 1
+
+  Constraints:
+
+  1 <= nums.length <= 20
+  0 <= nums[i] <= 1000
+  0 <= sum(nums[i]) <= 1000
+  -1000 <= target <= 1000
+
+"""
+
+
+class Solution:
+    def __init__(self):
+        self.currentCount = 0
+
+    def helper(self, nums: list[int], nextIndex: int, target: int, currentSum: int):
+        if nextIndex > len(nums):
+            return
+        if nextIndex == len(nums):
+            if currentSum == target:
+                self.currentCount = self.currentCount + 1
+            return
+        # include positive
+        self.helper(nums, nextIndex + 1, target, currentSum + nums[nextIndex])
+        # include negative
+        self.helper(nums, nextIndex + 1, target, currentSum - nums[nextIndex])
+
+    def findTargetSumWays(self, nums: list[int], target: int) -> int:
+        self.currentCount = 0
+        self.helper(nums, 0, target, 0)
+        return self.currentCount
+
+
+if __name__ == "__main__":
+    nums = [1, 1, 1, 1, 1]
+    target = 3
+    expected_output = 5
+    actual_output = Solution().findTargetSumWays(nums, target)
+    print(
+        f"nums={nums}, target={target} -> expected_output={expected_output}, actual_output={actual_output}"
+    )
