458f6554903d59e299751aea3207e48c545761c5
Nitin Garg <tintin.iitk@gmail.com>
2026-08-12 19:43:22 +0530

    python code for leetcode#1829 Maximum_xor_for_each_query

---
+++ em-april-2026/dsa/leetcode_practice_problems/unsorted/01829_Maximum_xor_for_each_query/main.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        n = len(nums)
+        m = maximumBit
+
+        MASK = 2**m - 1
+
+        def k_for_num(num: int) -> int:
+            return MASK ^ num
+
+        res = [0] * n
+        cumulative_xor = nums[0]
+        res[0] = k_for_num(cumulative_xor)
+        for i in range(1, n):
+            cumulative_xor = cumulative_xor ^ nums[i]
+            res[i] = k_for_num(cumulative_xor)
+        res.reverse()
+        return res
+
+
+def Test(nums: List[int], maximumBit: int, expected: int):
+    orig_nums = nums.copy()
+    print(f"[RUN]")
+    actual = Solution().getMaximumXor(nums, maximumBit)
+    assert (
+        actual == expected
+    ), f"  actual={actual} != expected={expected} for nums={orig_nums}, maximumBit={maximumBit}\n[FAILED]"
+    print(f"[DONE]")
+
+
+Test([0, 1, 1, 3], 2, [0, 3, 2, 3])
+Test([2, 3, 4, 7], 3, [5, 2, 6, 5])
