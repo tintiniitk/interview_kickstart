f0b847223d00d87dc45bd63c5f13494cc0a27115
Nitin Garg <tintin.iitk@gmail.com>
2026-08-14 19:04:04 +0530

    add test-run logs in leetcode#3310

--- em-april-2026/dsa/leetcode_practice_problems/unsorted/03310_remove_methods_from_project/main.py
+++ em-april-2026/dsa/leetcode_practice_problems/unsorted/03310_remove_methods_from_project/main.py
@@ -7,12 +7,16 @@ class Solution:
         self, n: int, k: int, invocations: List[List[int]]
     ) -> List[int]:
         dependency_methods = {}  # int -> set
+        in_degress = [0 for _ in range(n)]
+        out_degress = [0 for _ in range(n)]
         for invocation in invocations:
             dependent, dependency = invocation[0], invocation[1]
             if dependent not in dependency_methods:
                 dependency_methods[dependent] = set([dependency])
             else:
                 dependency_methods[dependent].add(dependency)
+            in_degress[dependency] += 1
+            out_degress[dependent] += 1
         suspicious_methods = set([k])
 
         # # expensive bfs with cyclicity-check
@@ -45,19 +49,29 @@ class Solution:
 
         full_set = set(range(n))
         remaining_methods = full_set - suspicious_methods
-        for remaining_method in remaining_methods:
-            if remaining_method in dependency_methods:
-                for dependency_method in dependency_methods[remaining_method]:
-                    if dependency_method in suspicious_methods:
-                        return list(full_set)
+
+        # # initially developed O(V+E) complexity approach for verification.
+        # for remaining_method in remaining_methods:
+        #     if remaining_method in dependency_methods:
+        #         for dependency_method in dependency_methods[remaining_method]:
+        #             if dependency_method in suspicious_methods:
+        #                 return list(full_set)
+
+        if sum([in_degress[i] for i in suspicious_methods]) != sum(
+            [out_degress[i] for i in suspicious_methods]
+        ):
+            return list(full_set)
+
         return list(remaining_methods)
 
 
 def Test(n: int, k: int, invocations: List[List[int]], expected: List[int]):
+    print(f"[RUN]\n  n={n}, k={k}, invocations={invocations}")
     actual = Solution().remainingMethods(n, k, invocations)
     assert sorted(actual) == sorted(
         expected
-    ), f"actual={actual}, expected={expected}, for n={n}, k={k}, invocations={invocations}"
+    ), f"actual={actual}, expected={expected}, for n={n}, k={k}, invocations={invocations}\n[FAILED]"
+    print("[DONE]")
 
 
 Test(4, 1, [[1, 2], [0, 1], [3, 2]], [0, 1, 2, 3])
