989290b85b4a85736960ffd1d70636f6f5377a12
Nitin Garg <tintin.iitk@gmail.com>
2026-09-09 14:07:37 +0530

    code ai mock interview 20260814

---
+++ em-april-2026/dsa/mock_interviews/ai/20260814/main.py
@@ -0,0 +1,90 @@
+from typing import List
+
+"""
+implement a stack using an array. Implement, push, pop, and peek.
+"""
+
+
+class myStack:
+    arr: List[int]
+    n: int
+    end: int = 0
+
+    def __init__(self, capacity: int = 10):
+        print(f"init({capacity})")
+        self.n = capacity
+        self.arr = [-1] * self.n
+        self.end = 0
+
+    def isEmpty(self):
+        return self.end == 0
+
+    def isFull(self):
+        return self.end == self.n
+
+    def peek(self) -> int:
+        print(f"peek()")
+        if self.end == 0:
+            raise ValueError(f"No entry in the stack")
+            # return -1
+        else:
+            return self.arr[self.end - 1]
+
+    def pop(self):
+        print(f"pop()")
+        if self.end == 0:
+            raise ValueError(f"No entry in the stack to pop")
+        else:
+            self.end -= 1
+
+    def push(self, value: int):
+        print(f"push({value})")
+        if self.end == self.n:
+            print(f"  resizing underlying array ...")
+            # need to resize, double in size.
+            new_arr = self.arr + [-1] * self.n
+            self.arr = new_arr
+            self.end = self.n
+            self.n *= 2
+        self.arr[self.end] = value
+        self.end += 1
+        return
+
+    def __str__(self) -> str:
+        return (
+            "MyStack : { "
+            + " -> ".join(list(map(lambda value: str(value), self.arr[: self.end])))
+            + " }"
+        )
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+# def Test(i: int):
+#     print(f"[RUN]")
+#     assert i % 2, f"i = {i}\n[FAILED]"
+#     print(f"[PASSED]")
+# Test(1)
+# Test(2)
+
+
+def Test():
+    s = myStack(1)
+    print(s)
+    s.push(10)
+    print(s)
+    s.push(8)
+    print(s)
+    print(s.peek())
+    s.pop()
+    print(s)
+    print(s.peek())
+    s.pop()
+    print(s)
+    s.pop()
+    print(s)
+    pass
+
+
+Test()
