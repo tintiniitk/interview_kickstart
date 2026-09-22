d67662071d075e76955721885990788dee7f28ec
Nitin Garg <tintin.iitk@gmail.com>
2026-09-22 15:50:23 +0530

    python code for 04050_Minimum_Days_to_Score_Exactly_N_Points

--- em-april-2026/dsa/leetcode_practice_problems/unsorted/04050_Minimum_Days_to_Score_Exactly_N_Points/main.py
+++ em-april-2026/dsa/leetcode_practice_problems/unsorted/04050_Minimum_Days_to_Score_Exactly_N_Points/main.py
@@ -1,53 +1,35 @@
-from functools import cache
-
-
 class Solution:
     def minDays(self, n: int) -> int:
-        # streak_sum_to_streak_size = [-1 for _ in range(n + 1)]
-        # streak_sum_to_streak_size[0] = 0
         streak_sum_to_streak_size = {0: 0}
         i = 1
         prev_streak_sum = 0
+        streak_sums = []
         while True:
             streak_sum = prev_streak_sum + i
             if streak_sum > n:
                 break
             streak_sum_to_streak_size[streak_sum] = i
+            streak_sums.append(streak_sum)
             prev_streak_sum = streak_sum
             i += 1
-        # dp = streak_sum_to_streak_size
-        # for s in range(2, n + 1):
-        #     if dp[s] == -1:
-        #         min_dp_s = s + 1
-        #         for t in range(s - 1, s // 2 - 1, -1):
-        #             min_dp_s = min(min_dp_s, dp[t] + 1 + dp[s - t])
-        #         dp[s] = min_dp_s
+
         dp = [-1 for _ in range(n + 1)]
         dp[0] = 0
         dp[1] = 1
         for s in range(2, n + 1):
-            if s not in streak_sum_to_streak_size:
-                for t in range(s - 1, s // 2 - 1, -1):
-                    if t in streak_sum_to_streak_size:
-                        dp[s] = streak_sum_to_streak_size[t] + 1 + dp[s - t]
+            if s not in streak_sums:
+                min_dp_s = 2 * s
+                for streak_sum in streak_sums:
+                    if streak_sum >= s:
                         break
+                    min_dp_s = min(
+                        min_dp_s,
+                        streak_sum_to_streak_size[streak_sum] + 1 + dp[s - streak_sum],
+                    )
+                dp[s] = min_dp_s
             else:
                 dp[s] = streak_sum_to_streak_size[s]
         return dp[n]
-
-        @cache
-        def min_days(s: int) -> int:
-            if dp[s] != -1:
-                return dp[s]
-            else:
-                min_dp_s = 2 * s
-                for t in range(s - 1, (s - 1) // 2, -1):
-                    min_dp_s = min(min_dp_s, min_days(t) + 1 + min_days(s - t))
-                if min_dp_s >= 2 * s:
-                    raise ValueError(f"could not find min_days for s = {s}")
-                return min_dp_s
-
-        return min_days(n)
 
 
 import sys
@@ -65,10 +47,12 @@ def Test(n: int, expected: int) -> tuple[bool, str]:
 
 
 def main():
-    sys.setrecursionlimit(5000)
     try:
         print("Running tests ...")
         with time_limit(5):
+            Test(n=2, expected=3)
+            Test(n=9, expected=6)
+            Test(n=12, expected=7)
             Test(n=1951, expected=77)
             Test(n=100000, expected=481)
     except TimeoutException as te:
