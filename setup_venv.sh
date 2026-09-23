b84cb6795a32a62b77e4b04ffc98b53a513455aa
Nitin Garg <tintin.iitk@gmail.com>
2026-09-23 10:48:13 +0530

    add venv support and some initial set of plugins

---
+++ setup_venv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get the absolute, fully dereferenced path of this script file
+SCRIPT_PATH=$(realpath "${BASH_SOURCE[0]:-$0}")
+# Get the absolute path of the directory containing this script
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+echo "Script File: $SCRIPT_PATH"
+echo "Script Directory: $SCRIPT_DIR"
+
+python3 -m venv ${SCRIPT_DIR}/.venv
+printf "Setting up python virtual environment in ${SCRIPT_DIR}/.venv now ...\n"
+source ${SCRIPT_DIR}/.venv/bin/activate
+yes | pip install fastapi uvicorn pydantic
+printf "\nThe script will come out of venv on exit. To reenter it, run the following command:\nsource ${SCRIPT_DIR}/.venv/bin/activate\n"
+printf "\nPlease run command **deactivate** to come out of it, when done.\n\nTo use this venv in VS code, please run Ctrl+Shift+P > Python: Select Interpreter > .venv/bin/python\n"
