e5eb3f4482eb9e5e962a30c2f6b42aae1fbc2999
Nitin Garg <tintin.iitk@gmail.com>
2026-09-23 18:56:09 +0530

    [todo-list web-app] updated scripts

--- interviews/202609-Blitzy/001-programming/prep/proj002-todo-list/delete_items.sh
+++ interviews/202609-Blitzy/001-programming/prep/proj002-todo-list/delete_items.sh
@@ -2,11 +2,19 @@
 set -e
 source $(dirname $0)/utils.sh
 
-if [[ $# > 0 ]] ; then
-    >&2 echo "No argument expected"
+if [[ $# > 1 ]] ; then
+    >&2 echo "At the most 1 arguments (both optional) expected: [<status>]"
     exit 1
+fi
+INPUT_DATA=""
+if [[ $# > 0 ]] ; then
+    status=${1}
+    INPUT_DATA+="\\\"status\\\": \\\"${status}\\\""
+fi
+if test -n "${INPUT_DATA}" ; then
+    INPUT_DATA="-H \"Content-Type: application/json\" -d \"{${INPUT_DATA}\"}"
 fi
 
 post_route='/items'
 items_url=${LOCALHOST}${post_route}
-run_and_log "curl -X DELETE \"${items_url}\""
+run_and_log "curl -X DELETE ${INPUT_DATA} \"${items_url}\""
--- interviews/202609-Blitzy/001-programming/prep/proj002-todo-list/get_items.sh
+++ interviews/202609-Blitzy/001-programming/prep/proj002-todo-list/get_items.sh
@@ -2,11 +2,23 @@
 set -e
 source $(dirname $0)/utils.sh
 
-if [[ $# > 0 ]] ; then
-    >&2 echo "No argument expected"
+if [[ $# > 2 ]] ; then
+    >&2 echo "At the most 2 arguments (both optional) expected: [<limit>=0 [<status>=null]]"
     exit 1
+fi
+INPUT_DATA=""
+if [[ $# > 0 ]] ; then
+    limit=${1}
+    INPUT_DATA+="\\\"limit\\\": ${limit}"
+    if [[ $# > 1 ]] ; then
+        status=${2}
+        INPUT_DATA+=", \\\"status\\\": \\\"${status}\\\""
+    fi
+fi
+if test -n "${INPUT_DATA}" ; then
+    INPUT_DATA="-H \"Content-Type: application/json\" -d \"{${INPUT_DATA}\"}"
 fi
 
 post_route='/items'
 get_url=${LOCALHOST}${post_route}
-run_and_log "curl -X GET -H \"Content-Type: application/json\" \"${get_url}\""
+run_and_log "curl -X GET ${INPUT_DATA} \"${get_url}\""
