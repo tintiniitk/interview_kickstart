90c87b0282e911d01f86b99daa2bcdc0585ca251
Nitin Garg <tintin.iitk@gmail.com>
2026-09-24 15:59:28 +0530

    add module sortedcontainer to virtual environment

--- setup_venv.sh
+++ setup_venv.sh
@@ -11,6 +11,6 @@ python3 -m venv "${SCRIPT_DIR}"/.venv
 printf "Setting up python virtual environment in %s/.venv now ...\n" "${SCRIPT_DIR}"
 # shellcheck disable=SC1091
 source "${SCRIPT_DIR}"/.venv/bin/activate
-yes | pip install fastapi uvicorn pydantic
+yes | pip install fastapi uvicorn pydantic sortedcontainers
 printf "\nThe script will come out of venv on exit. To reenter it, run the following command:\nsource %s/.venv/bin/activate\n" "${SCRIPT_DIR}"
 printf "\nPlease run command **deactivate** to come out of it, when done.\n\nTo use this venv in VS code, please run Ctrl+Shift+P > Python: Select Interpreter > .venv/bin/python\n"
